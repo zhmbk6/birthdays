cde44db6760bd74cb24d8093bfb8138f7bb5eae0
zhmbk6 <5tk45r9zhs@privaterelay.appleid.com>
2026-07-31 17:20:35 +0500

    First Birthday

---
+++ script.js
@@ -0,0 +1,132 @@
+const images = document.querySelectorAll(".gallery-container img");
+
+const lightbox = document.querySelector(".lightbox");
+
+const lightboxImage = document.querySelector(".lightbox-image");
+
+const close = document.querySelector(".close");
+
+images.forEach(image=>{
+
+    image.addEventListener("click",()=>{
+
+        lightbox.style.display="flex";
+
+        lightboxImage.src=image.src;
+
+    });
+
+});
+
+close.addEventListener("click",()=>{
+
+    lightbox.style.display="none";
+
+});
+
+lightbox.addEventListener("click",(e)=>{
+
+    if(e.target===lightbox){
+
+        lightbox.style.display="none";
+
+    }
+
+});
+const startDate = new Date("2024-07-27T00:00:00");
+
+function updateCounter(){
+
+    const now = new Date();
+
+    const difference = now - startDate;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    const hours = now.getHours();
+
+    const minutes = now.getMinutes();
+
+    const seconds = now.getSeconds();
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+}
+
+updateCounter();
+
+setInterval(updateCounter,1000);
+const reasons = [
+
+"❤️ Потому что рядом с тобой я счастлив.",
+
+"🌸 Потому что твоя улыбка делает мой день лучше.",
+
+"🤍 Потому что ты понимаешь меня.",
+
+"🥹 Потому что ты заботишься обо мне.",
+
+"💖 Потому что ты делаешь мою жизнь ярче.",
+
+"🌹 Потому что с тобой хочется строить будущее.",
+
+"✨ Потому что рядом с тобой я настоящий.",
+
+"💍 Потому что я вижу тебя в своем будущем.",
+
+"🌙 Потому что ты — мой любимый человек.",
+
+"❤️ Потому что я просто люблю тебя."
+
+];
+
+let index = 0;
+
+const reasonText = document.getElementById("reasonText");
+const reasonCount = document.getElementById("reasonCount");
+
+document.getElementById("nextReason").addEventListener("click", () => {
+
+    reasonCount.style.display = "block";
+
+    reasonText.style.opacity = 0;
+
+    setTimeout(() => {
+
+        reasonText.textContent = reasons[index];
+
+        reasonCount.textContent = `Причина ${index + 1} из ${reasons.length}`;
+
+        reasonText.style.opacity = 1;
+
+        index++;
+if(index >= reasons.length){
+
+    document.getElementById("nextReason").style.display = "none";
+
+    reasonCount.style.display = "none";
+
+    setTimeout(()=>{
+
+        reasonText.style.display = "none";
+
+        const finalMessage = document.getElementById("finalMessage");
+
+        finalMessage.style.display = "block";
+
+        setTimeout(()=>{
+
+            finalMessage.style.opacity = "1";
+
+        },100);
+
+    },700);
+
+}
+
+    }, 200);
+
+});
