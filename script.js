57786f685c4b11b53e3a4fbbe3a0c8abe484bc82
zhmbk6 <5tk45r9zhs@privaterelay.appleid.com>
2026-08-01 12:18:57 +0500

    reedit

--- script.js
+++ script.js
@@ -182,3 +182,26 @@ function createHeart(){
     },3000);
 
 }
+const galleryImages = document.querySelectorAll(".gallery-container img");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+galleryImages.forEach(image=>{
+
+    observer.observe(image);
+
+});
